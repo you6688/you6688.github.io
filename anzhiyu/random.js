29125334399ab07613c33ee0cb65b07e9302cf46
you6688 <youvcd@163.com>
2025-08-07 22:32:51 +0800

    Site updated: 2025-08-07 22:32:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/hello-world/","2025/08/07/这是一篇新的博文/","2025/08/07/这是黄先生的篇新的博文/"];function toRandomPost(){
+var posts=["2025/08/07/这是一篇新的博文-2/","2025/08/07/这是一篇新的博文-1/","2025/08/07/这是一篇新的博文/","2025/08/07/这是黄先生的篇新的博文/","2025/08/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
